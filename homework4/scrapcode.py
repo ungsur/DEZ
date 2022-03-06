2f264dbb72ed25db8e2c52bb80588fc8d5e782dd
Richard Ungsunan <ungsunan@gmail.com>
2022-03-06 18:06:18 -0500

    hw4

---
+++ homework4/scrapcode.py
@@ -0,0 +1,11 @@
+StructType(
+    List(
+        StructField('hvfhs_license_num',StringType,True),
+        StructField('dispatching_base_num',StringType,True),
+        StructField('pickup_datetime',TimeStampType,True),
+        StructField('dropoff_datetime',TimeStampType,True),
+        StructField('PULocationID',IntegerType,True),
+        StructField('DOLocationID',IntegerType,True),
+        StructField('SR_Flag',StringType,True)
+        )
+    )
